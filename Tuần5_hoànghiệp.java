9a806298e6f7eefae0e1530273fcee66b49984d4
hoanghiep150106 <thaihoanghiephh@gmail.com>
2025-10-05 18:09:25 +0700

    Add files via upload

---
+++ Tuần5_hoànghiệp.java
@@ -0,0 +1,6 @@
+public class Tuần5_hoànghiệp {
+    public static void main(String[] args) {
+        sach S1 = new sach("a987"," không phải sói nhưng cũng đừng là cừu","Lê Bảo Ngọc", 2023, 6000);
+        S1.hienThiThongTin();
+    }
+}
